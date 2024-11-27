b331ee46acd54feae291a33e9d2ac3362fd39d96
Soheima Maradiaga <soheimam@gmail.com>
2024-11-27 15:39:30 +0100

    removed additional request

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -10,19 +10,60 @@ const USDC_ABI = [
 ];
 
 
-const webhookSecret = process.env.CLERK_SIGNING_KEY || ``
 
-async function validateRequest(request: Request) {
-    const payloadString = await request.text()
-    const headerPayload = await headers()
 
-    const svixHeaders = {
-        'svix-id': headerPayload.get('svix-id')!,
-        'svix-timestamp': headerPayload.get('svix-timestamp')!,
-        'svix-signature': headerPayload.get('svix-signature')!,
+export async function POST(req: Request) {
+    const SIGNING_SECRET = process.env.CLERK_SIGNING_KEY
+
+    if (!SIGNING_SECRET) {
+        throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
     }
-    const wh = new Webhook(webhookSecret)
-    return wh.verify(payloadString, svixHeaders) as WebhookEvent
+
+    // Create new Svix instance with secret
+    const wh = new Webhook(SIGNING_SECRET)
+
+    // Get headers
+    const headerPayload = await headers()
+    const svix_id = headerPayload.get('svix-id')
+    const svix_timestamp = headerPayload.get('svix-timestamp')
+    const svix_signature = headerPayload.get('svix-signature')
+
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response('Error: Missing Svix headers', {
+            status: 400,
+        })
+    }
+
+    // Get body
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
+
+    let evt: WebhookEvent
+
+    // Verify payload with headers
+    try {
+        evt = wh.verify(body, {
+            'svix-id': svix_id,
+            'svix-timestamp': svix_timestamp,
+            'svix-signature': svix_signature,
+        }) as WebhookEvent
+    } catch (err) {
+        console.error('Error: Could not verify webhook:', err)
+        return new Response('Error: Verification error', {
+            status: 400,
+        })
+    }
+
+    // Do something with payload
+    await HandleTransfer(evt as unknown as ClerkPayload);
+    // For this guide, log payload to console
+    const { id } = evt.data
+    const eventType = evt.type
+    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+    console.log('Webhook payload:', body)
+
+    return new Response('Webhook received', { status: 200 })
 }
 
 
@@ -31,14 +72,14 @@ export async function GET(request: Request) {
     return NextResponse.json({ message: 'Hello from Next.js!' })
 }
 
-export async function POST(request: Request) {
-    const event = await validateRequest(request) as unknown as ClerkPayload;
-    console.log(JSON.stringify(event, null, 2));
+export async function HandleTransfer(event: ClerkPayload) {
+
     // const body = await request.json() as ClerkPayload;
 
 
     // Validate request came from Clerk
     const wallet = event.data.web3_wallets[0].web3_wallet;
+    console.log(`wallet: ${wallet}`);
     if (!wallet) {
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
@@ -56,6 +97,7 @@ export async function POST(request: Request) {
         // Transfer 1 USDC
         const usdcContract = new ethers.Contract(USDC_CONTRACT_ADDRESS, USDC_ABI, signer);
         const tx = await usdcContract.transfer(wallet, ethers.parseUnits("1", 6));
+        console.log(`tx: ${tx}`);
         await tx.wait();
 
         return NextResponse.json({
