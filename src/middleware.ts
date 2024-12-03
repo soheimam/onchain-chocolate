a73ff0cd0a79103ce1cd3ab80fd0b6c1027c2277
Soheima Maradiaga <soheimam@gmail.com>
2024-12-03 14:20:15 -0800

    removed store error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-    // Routes that can be accessed while signed out
-    publicRoutes: ["/", "/sign-in", "/sign-up"],
-    // Routes that can always be accessed, and have
-    // no authentication information
-    ignoredRoutes: ["/api/signup"],
-});
+const isProtectedRoute = createRouteMatcher(['/store(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+    if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+}
